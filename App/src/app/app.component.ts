191106c3500ec3900e5ac06669d17cf01022b67d
Kengo Yamashita <63894988+yamashita-kenngo@users.noreply.github.com>
2023-07-09 01:49:13 +0900

    ヘッダーとサイドバーメニューを追加 (#19)
    * chore: add taiga ui
    
    * feat: add header
    
    * feat: add header and menu
    
    * chore: add todo comment

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'App';
+  title = 'Tenant App';
+  open = false;
+  toggle(open: boolean) {
+    this.open = open;
+  }
+  items: readonly { label: string; icon: string }[] = [
+    {
+      label: '申請管理',
+      icon: 'tuiIconCalendarLarge',
+    },
+    {
+      label: '施設管理',
+      icon: 'tuiIconHomeLarge',
+    },
+    {
+      label: '団体管理',
+      icon: 'tuiIconUsersLarge',
+    },
+    {
+      label: '会計管理',
+      icon: 'tuiIconDollarSignLarge',
+    },
+    {
+      label: '規約管理',
+      icon: 'tuiIconArchiveLarge',
+    },
+    {
+      label: '運営会議',
+      icon: 'tuiIconSmileLarge',
+    },
+    {
+      label: '報告書作成',
+      icon: 'tuiIconTrelloLarge',
+    },
+    {
+      label: '設定',
+      icon: 'tuiIconSettingsLarge',
+    },
+  ];
+  order = new Map();
 }
--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -1,3 +1,19 @@
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TUI_SANITIZER,
+  TuiLabelModule,
+  TuiButtonModule,
+  TuiSvgModule,
+} from '@taiga-ui/core';
+import { TuiAppBarModule } from '@taiga-ui/addon-mobile';
+import { TuiBlockStatusModule } from '@taiga-ui/layout';
+import { TuiTilesModule } from '@taiga-ui/kit';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TuiSidebarModule } from '@taiga-ui/addon-mobile';
+import { TuiActiveZoneModule } from '@taiga-ui/cdk';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,14 +21,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiAppBarModule,
+    TuiBlockStatusModule,
+    TuiLabelModule,
+    TuiButtonModule,
+    TuiSidebarModule,
+    TuiActiveZoneModule,
+    TuiTilesModule,
+    TuiSvgModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

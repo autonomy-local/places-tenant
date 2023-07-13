2a761c7072fdbfccc1a0c442507bbe6f07bd5785
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-07-13 21:45:00 +0900

    feat: add account

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'reservation', loadChildren: () => import('./reservation/reservation.module').then(m => m.ReservationModule) }, { path: 'place', loadChildren: () => import('./place/place.module').then(m => m.PlaceModule) }, { path: 'group', loadChildren: () => import('./group/group.module').then(m => m.GroupModule) }];
+const routes: Routes = [{ path: 'reservation', loadChildren: () => import('./reservation/reservation.module').then(m => m.ReservationModule) }, { path: 'place', loadChildren: () => import('./place/place.module').then(m => m.PlaceModule) }, { path: 'group', loadChildren: () => import('./group/group.module').then(m => m.GroupModule) }, { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

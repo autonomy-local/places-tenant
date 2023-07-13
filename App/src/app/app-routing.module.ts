28d7fb90b59bc367f041e5a4e81e9be6ee139266
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-07-13 21:43:08 +0900

    feat: add place template

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'reservation', loadChildren: () => import('./reservation/reservation.module').then(m => m.ReservationModule) }];
+const routes: Routes = [{ path: 'reservation', loadChildren: () => import('./reservation/reservation.module').then(m => m.ReservationModule) }, { path: 'place', loadChildren: () => import('./place/place.module').then(m => m.PlaceModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

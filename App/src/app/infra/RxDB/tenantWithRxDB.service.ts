3216ff2850b0422015edc53aaece0d60c615c5dd
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-07-20 23:08:14 +0900

    chore: add tenantWithRxDB Service file

---
+++ App/src/app/infra/RxDB/tenantWithRxDB.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TenantWithRXDBService {
+  constructor() {}
+
+  async insertTenant() {
+    // TODO
+  }
+
+  async findAllTenants() {
+    // TODO
+  }
+}

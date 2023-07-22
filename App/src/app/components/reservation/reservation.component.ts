e86f20a6d6503c197da79d6c4e0de02454285fbd
Kengo Yamashita <63894988+yamashita-kenngo@users.noreply.github.com>
2023-07-22 20:07:15 +0900

    feat: add calendar ui (#93)

--- App/src/app/components/reservation/reservation.component.ts
+++ App/src/app/components/reservation/reservation.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
-  styleUrls: ['./reservation.component.less']
+  styleUrls: ['./reservation.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit {
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
+    events: [
+      { title: 'event 1', date: '2023-07-22' },
+      { title: 'event 2', date: '2023-07-25' },
+      { title: 'event 3', date: '2023-07-26' },
+      { title: 'event 4', date: '2023-07-27' },
+    ],
+  };
 
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
 }
--- App/src/app/components/reservation/reservation.module.ts
+++ App/src/app/components/reservation/reservation.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ReservationRoutingModule } from './reservation-routing.module';
 import { ReservationComponent } from './reservation.component';
 
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { TuiBlockStatusModule } from '@taiga-ui/layout';
 
 @NgModule({
-  declarations: [
-    ReservationComponent
-  ],
+  declarations: [ReservationComponent],
   imports: [
     CommonModule,
-    ReservationRoutingModule
-  ]
+    ReservationRoutingModule,
+    FullCalendarModule,
+    TuiBlockStatusModule,
+  ],
 })
-export class ReservationModule { }
+export class ReservationModule {}

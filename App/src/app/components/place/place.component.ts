9db7266ad780fde31a9c58914f44f04747aee6a8
Kengo Yamashita <63894988+yamashita-kenngo@users.noreply.github.com>
2023-07-22 14:05:27 +0900

    feat: add places list (#82)

--- App/src/app/components/place/place.component.ts
+++ App/src/app/components/place/place.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { PlaceWithRXDBService } from '../../infra/RxDB/placeWithRxDB.service';
+import { Place } from '../../infra/domain-model/place.model';
 
 @Component({
   selector: 'app-place',
   templateUrl: './place.component.html',
-  styleUrls: ['./place.component.less']
+  styleUrls: ['./place.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlaceComponent {
-
+export class PlaceComponent implements OnInit {
+  constructor(public placeWithRXDBService: PlaceWithRXDBService) {}
+  places: Place[] = [];
+  columns = ['名称', '住所'];
+  places$ = signal(this.places);
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.placeWithRXDBService.findAllPlaces().then((places) => {
+      if (places) {
+        this.places = places.map((place) => place._data) as Place[];
+        console.log(this.places);
+        this.places$.set(this.places);
+      }
+    });
+  }
 }
--- App/src/app/components/place/place.module.ts
+++ App/src/app/components/place/place.module.ts
@@ -3,15 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { PlaceRoutingModule } from './place-routing.module';
 import { PlaceComponent } from './place.component';
-
+import { TuiTableModule } from '@taiga-ui/addon-table';
+import { TuiBlockStatusModule } from '@taiga-ui/layout';
+import { TuiStepperModule } from '@taiga-ui/kit';
+import { TuiFormatNumberPipeModule } from '@taiga-ui/core';
+import { TuiTagModule } from '@taiga-ui/kit';
+import { TuiNotificationModule } from '@taiga-ui/core';
+import { TuiButtonModule } from '@taiga-ui/core';
 
 @NgModule({
-  declarations: [
-    PlaceComponent
-  ],
+  declarations: [PlaceComponent],
   imports: [
     CommonModule,
-    PlaceRoutingModule
-  ]
+    PlaceRoutingModule,
+    TuiTableModule,
+    TuiFormatNumberPipeModule,
+    TuiTagModule,
+    TuiBlockStatusModule,
+    TuiStepperModule,
+    TuiNotificationModule,
+    TuiButtonModule,
+  ],
 })
-export class PlaceModule { }
+export class PlaceModule {}

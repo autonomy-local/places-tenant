8f79d8dd7cde3c80ac180d1e6fbbd25a962865a1
Kengo Yamashita <63894988+yamashita-kenngo@users.noreply.github.com>
2023-07-23 10:18:38 +0900

    fix: design (#96)

--- App/src/app/components/reservation/reservation.component.ts
+++ App/src/app/components/reservation/reservation.component.ts
@@ -12,6 +12,32 @@ export class ReservationComponent implements OnInit {
   calendarOptions: CalendarOptions = {
     plugins: [dayGridPlugin],
     initialView: 'dayGridMonth',
+    showNonCurrentDates: false,
+    timeZone: 'local',
+    locale: 'ja',
+    initialDate: new Date(),
+    headerToolbar: {
+      right: 'selectPlaceButton addEventButton prev,next',
+    },
+    buttonText: {
+      month: '月',
+      week: '週',
+      day: '日',
+    },
+    customButtons: {
+      addEventButton: {
+        text: '予約を追加',
+        click: () => {
+          console.log('addEventButton');
+        },
+      },
+      selectPlaceButton: {
+        text: '場所を選択',
+        click: () => {
+          console.log('selectPlaceButton');
+        },
+      },
+    },
     events: [
       { title: 'event 1', date: '2023-07-22' },
       { title: 'event 2', date: '2023-07-25' },
